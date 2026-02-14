576f7b78da6bfaa7d7b1dfc890a5cb79d69f53a6
pdawson2 <pdawson2@oberlin.edu>
2026-02-14 18:42:41 -0500

    I want to be done

--- Excel_Generator.py
+++ Excel_Generator.py
@@ -29,7 +29,7 @@ def generate_input_file(fte_eq: int,
     ws = wb.create_sheet(title="hours-per-item")
     generate_hours_per(ws, permit_catagories, review_disciplines)
 
-    wb.save('outplut.xlsx')
+    wb.save('input-sheet.xlsx')
 
     return 0
 
@@ -42,6 +42,7 @@ def generate_overall_information(ws: openpyxl.worksheet, fte_eq : int, departmen
     for i in range (len(departments)):
         ws.cell(row = 2+i, column = 1, value =  departments[i][0])
         ws.cell(row = 2+i, column = 2, value =  departments[i][1])
+    adjust_ws_width(ws)
     return ws
 
 
@@ -62,6 +63,8 @@ def generate_types_per_interval(ws: openpyxl.worksheet, permit_catagories: list[
         for i in range(catagory[1]):
             ws.cell(row = row + i, column = 2, value = "-")
         row += catagory[1]
+    adjust_ws_width(ws)
+    return ws
 
 def generate_hours_per(ws: openpyxl.worksheet, permit_catagories: list[tuple[str, int]], review_disciplines: list[str]):
     ws["A1"] = "Number of Review Disciplines"
@@ -81,5 +84,16 @@ def generate_hours_per(ws: openpyxl.worksheet, permit_catagories: list[tuple[str
     for discipline in review_disciplines:
         ws.cell(row = 2, column = col, value = discipline)
         col += 1
+    adjust_ws_width(ws)
+    return ws
 
 
+def adjust_ws_width(ws: openpyxl.worksheet) :
+    dims = {}
+    for row in ws.rows:
+        for cell in row:
+            if cell.value:
+                dims[cell.column_letter] = max((dims.get(cell.column_letter, 0), len(str(cell.value))))    
+    for col, value in dims.items():
+        ws.column_dimensions[col].width = value
+    
